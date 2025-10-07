335b60617caba4e4217f9a546e6d6bbb79c6c38c
Khanhdang225514 <dang225514@student.nctu.edu.vn>
2025-10-07 10:28:05 +0700

    Mô tả ngắn gọn thay đổi

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// Cho phép frontend gọi API backend
+app.use(cors());
 
 // Middleware đọc dữ liệu JSON
 app.use(express.json());
@@ -8,5 +12,6 @@ app.use(express.json());
 const userRoutes = require("./routes/user");
 app.use("/users", userRoutes);
 
+// Chạy server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
