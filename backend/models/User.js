5a9b895b03b9479ef8d1a799b11fbfa26a120ff6
tthqndvntc <nguyentronghieutc2004@gmail.com>
2025-10-21 08:35:58 +0700

    Feat: Update User schema for authentication

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,4 @@
-// backend/models/User.js
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -11,6 +10,15 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true, // Email không được trùng
   },
-});
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user', // Mặc định tài khoản mới là 'user'
+  }
+}, { timestamps: true });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
